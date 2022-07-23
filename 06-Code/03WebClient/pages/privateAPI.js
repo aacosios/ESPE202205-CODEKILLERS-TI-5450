68b8b119b81d51a33e024ea498711022501d596f
Pamela Flores Medrano <plflores2@espe.edu.ec>
2022-07-22 20:37:58 -0500

    Create privateAPI.js

---
+++ 06-Code/03WebClient/pages/privateAPI.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const privateAPI = () => {
+  return (
+    <div>Private API coming soon...</div>
+  )
+}
+
+export default privateAPI
+
+import axios from 'axios';
+/*
+const publicAPI = ({ userList }) => <div style={{ margin: 20 }}>
+  <h3>Next.js How to call API and display records in table</h3>
+  <table border="1">
+    <thead>
+      <th>ID</th>
+      <th>Journal</th>
+      <th>eissn</th>
+      <th>publication_date</th>
+      <th>article_type</th>
+      <th>author_display</th>
+      <th>title_display</th>
+      <th>score</th>
+    </thead>
+    <tbody>
+      {userList.data.map((x, i) => <tr key={i}>
+        <td>{x.id}</td>
+        <td>{x.journal}</td>
+        <td>{x.eissn}</td>
+        <td>{x.publication_date}</td>
+        <td>{x.article_type}</td>
+        <td>{x.author_display}</td>
+        <td>{x.title_display}</td>
+        <td>{x.score}</td>
+      </tr>)}
+    </tbody>
+  </table>
+</div>
+  
+publicAPI.getInitialProps = async () => {
+  const { data } = await axios.get('https://api.plos.org/search?q=title:github');
+  return { userList: data };
+}
+  
+
+export default publicAPI;
+*/
