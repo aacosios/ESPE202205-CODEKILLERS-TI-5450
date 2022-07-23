3931ba39281994beb3ca62647718efd8788d0812
Pamela Flores Medrano <plflores2@espe.edu.ec>
2022-07-22 20:39:52 -0500

    Create about.js

---
+++ 06-Code/03WebClient/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Abstract from '../components/Abstract'
+
+const about = () => {
+  return (
+    <div>ABOUT PAGE IN PROCESS...
+      <div><Abstract/></div>
+    </div>
+    
+
+  )
+}
+
+export default about
