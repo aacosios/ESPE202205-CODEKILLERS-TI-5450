26b6a8d70b24fdec2820fc2a03952861c489fb31
ALEXIS FABIAN  CORREA GALLEGOS <84604264+afcorrea0964@users.noreply.github.com>
2022-07-25 15:20:02 -0500

    Add files via upload

---
+++ 06-Code/03WebClient/components/abstracts/Fifth.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+
+export default function Fourth() {
+  const [abstracts, setAbstracts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.plos.org/search?q=id:10.1371/journal.pcbi.1007142")
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response.response.docs);
+        setAbstracts(response.response.docs);
+      });
+  }, []);
+
+  const columns = [
+    { field: "abstract", headerName: "ABSTRACT", width: 600 },
+  ];
+
+  return (
+    <Box
+      sx={{
+        width: "70%",
+        height: "82vh",
+        marginLeft: "auto",
+        marginRight: "auto",
+        justifyContent: "center",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        paddingLeft: "20px",
+        paddingRight: "20px",
+        background: "#fff",
+        borderRadius: "15px",
+        boxShadow: "1px 1px 20px #333",
+      }}
+    >
+      <h1>Correction: Ten Simple Rules for Taking Advantage of Git and GitHub</h1>
+      <br />
+
+      <div style={{ height: 800, width: "100%" }}>
+      <DataGrid
+      getRowHeight={() => 'auto'} getEstimatedRowHeight={() => 500}
+                    rows=
+                    {
+                        abstracts.map(item => (
+                            
+                            {
+                                id: item.id,
+                                abstract: item.abstract
+                            }
+                            
+                        )) 
+                    }
+                    
+                    columns={columns}
+                    pageSize={1}
+                    rowsPerPageOptions={[1]}
+                />
+      </div>
+    </Box>
+  );
+
+   
+}
+
