56fdd1463eab8395e05902708e4c74f1d9c022ab
Michael23-OSS <66226118+Michael23-OSS@users.noreply.github.com>
2022-07-26 02:40:54 -0500

    Delete Login.js

--- 06-Code/03WebClient/pages/login/src/Login.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-
-export const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button style={{backgroundColor: 'teal', borderColor: 'aqua', color: 'silver',}} onClick={() => loginWithRedirect()}>Login</button>;
-};
