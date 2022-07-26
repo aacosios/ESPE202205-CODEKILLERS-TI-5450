d2ca466962550fbe1b16af88e22244d93092f5ec
Michael23-OSS <maespinosa10@espe.edu.ec>
2022-07-26 02:46:50 -0500

    Update App.js

--- 06-Code/03WebClient/pages/login/src/App.js
+++ 06-Code/03WebClient/pages/login/src/App.js
@@ -4,7 +4,7 @@ import { LoginButton } from "./Login";
 import { LogoutButton } from "./Logout";
 import { Profile } from "./Profile";
 import logo from "./logo.svg";
-import "./App.module.css";
+import "./App.css";
 
 function App() {
   const { isAuthenticated } = useAuth0();
@@ -17,6 +17,7 @@ function App() {
           <>
             <Profile />
             <LogoutButton />
+            
           </>
         ) : (
           <LoginButton />
