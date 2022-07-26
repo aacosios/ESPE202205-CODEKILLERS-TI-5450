2d71b788e6aee5216c0140ca9eb5f292c5c1a5d3
aacosios <aacosios@espe.edu.ec>
2022-07-25 22:18:36 -0500

    coment

--- 06-Code/03WebClient/pages/_app.js
+++ 06-Code/03WebClient/pages/_app.js
@@ -1,12 +1,6 @@
 import Layout	 from '../components/Layout'
 import '../styles/globals.css'
-/*import '../styles/index.css'
-import '../styles/Footer.module.css'
-import '../styles/Home.module.css'
-import '../styles/ItemsDelivery.module.css'
-import '../styles/ItemsList.module.css'
-import '../styles/Navbar.module.css'
-*/
+
 
 function MyApp({ Component, pageProps }) {
   return (
