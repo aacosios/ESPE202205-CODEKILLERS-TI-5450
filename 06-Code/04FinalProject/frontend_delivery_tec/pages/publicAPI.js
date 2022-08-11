27e80d9ca2c8d61e9840ed3fc9fdf5200c5031d2
Pamela Flores Medrano <plflores2@espe.edu.ec>
2022-08-11 12:53:29 -0500

    Create publicAPI.js

---
+++ 06-Code/04FinalProject/frontend_delivery_tec/pages/publicAPI.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TablePapers from '../components/TablePapers';
+
+const publicAPI = () => {
+
+  return (
+  
+    <div><TablePapers/></div>
+    
+  )
+}
+
+export default publicAPI
+
