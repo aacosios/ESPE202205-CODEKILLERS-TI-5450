92536fd0c572ecfcbee1db1433fd6b46119360d3
Pamela Flores Medrano <plflores2@espe.edu.ec>
2022-08-11 12:46:31 -0500

    Create AboutImage.jsx

---
+++ 06-Code/04FinalProject/frontend_delivery_tec/components/AboutImage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from "next/image";
+
+
+const AboutImage = () => {
+  return (
+    <div>
+
+<center>
+<Image src="/img/deliv.png" alt="" width="300" height="300" />
+</center>
+
+    </div>
+  )
+}
+
+export default AboutImage
