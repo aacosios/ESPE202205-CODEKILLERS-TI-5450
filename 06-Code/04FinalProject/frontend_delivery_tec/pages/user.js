76b177b1c92e772fb9b778f079b39a9017ab821e
Pamela Flores Medrano <plflores2@espe.edu.ec>
2022-08-11 12:52:50 -0500

    Create user.js

---
+++ 06-Code/04FinalProject/frontend_delivery_tec/pages/user.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Users from '../components/CRUD/User';
+
+
+const user = () => {
+  return (
+    <div>
+        <Users></Users>
+    </div>
+  )
+}
+
+export default user
