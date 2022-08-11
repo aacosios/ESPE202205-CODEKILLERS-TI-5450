f06436cd1d061e7406d789d717dc187084a8dd82
Pamela Flores Medrano <plflores2@espe.edu.ec>
2022-08-11 12:46:33 -0500

    Create About.js

---
+++ 06-Code/04FinalProject/frontend_delivery_tec/components/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from "../styles/about.module.css";
+
+
+export function About() {
+  return (
+    <div>
+
+<header>
+        <h1 className={styles.title}>About us </h1>
+      </header>
+
+        <body>
+            <center>
+<font size="4"> Nuestro objetivo es apoyar a la economía ecuatoriana 
+cuando inician o experimentan una transformación tecnológica importante.
+Si pensabas que los delivery solo hacían entrega de comida, pues 
+¡estás equivocado! Si algo sabemos es que la pandemia ha incrementado 
+y brotado las ideas de todos para hacer emprendimientos innovadores. 
+Uno de ellos son los delivery de tecnología.</font>
+</center>
+        </body>
+      
+
+    </div>
+  );
+}
+
+export default About
