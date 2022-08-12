95d2e1fd87e72d96912e70e317450fd1c14984cf
aacosios <aacosios@espe.edu.ec>
2022-08-11 22:55:03 -0500

    clear

--- 06-Code/04FinalProject/frontend_delivery_tec/pages/_app.js
+++ 06-Code/04FinalProject/frontend_delivery_tec/pages/_app.js
@@ -5,13 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import Product from './product'
 import ReactDOM from 'react-dom';
 
-/*import '../styles/index.css'
-import '../styles/Footer.module.css'
-import '../styles/Home.module.css'
-import '../styles/ItemsDelivery.module.css'
-import '../styles/ItemsList.module.css'
-import '../styles/Navbar.module.css'
-*/
+
 import React from 'react';
 
 
